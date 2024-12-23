ad12b0d9fbf8bafe7de768c85b0486ff64d229d3
Aijaz Bin Qasim <aijazbinqasim@hotmail.com>
2024-12-23 22:24:16 +0500

    Add project files.

---
+++ Demo.Extension.Method/ExtensionMethods/HelperExtensions.cs
@@ -0,0 +1,35 @@
+﻿namespace Demo.Extension.Method.ExtensionMethods
+{
+    public static class HelperExtensions
+    {
+        public static double Sum(this int value1, int value2)
+        {
+            return value1 + value2;
+        }
+
+
+        public static string Concat(this string str1, string str2)
+        {
+            return $"{str1} - {str2}";
+        }
+
+
+        public static void Print(this Action<int> action, int val)
+        {
+            action(val);
+        }
+
+
+        public static void ValidateAndExecute(this Action<int> action, int val, Func<int, bool> validator)
+        {
+            if (validator(val))
+            {
+                action(val);
+            }
+            else
+            {
+                Console.WriteLine($"Validation failed for value: {val}");
+            }
+        }
+    }
+}
---
+++ Demo.Extension.Method/Program.cs
@@ -0,0 +1,37 @@
+﻿using Demo.Extension.Method.ExtensionMethods;
+
+namespace Demo.Extension.Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            //var val = 10;
+
+            //var result = val.Sum(20);
+
+
+            //Console.WriteLine(result);
+
+
+            //var name = "Walidad";
+            //Console.WriteLine(name.Concat("Brohi"));
+
+            //Action<int> action = val => Console.WriteLine(val);
+            //action.Print(100);
+
+
+
+            // Define an Action<int>
+            Action<int> print = x => Console.WriteLine($"Printing: {x}");
+
+            // Call the extension method with validation
+            print.ValidateAndExecute(10, x => x > 0); // Valid
+            print.ValidateAndExecute(-5, x => x > 0); // Invalid
+        }
+    }
+}
